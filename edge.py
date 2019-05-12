a7ac43dbafd7135b931902c7378c1f5fb0f028d8
Theo <lucastheo.g.a@gmail.com>
2019-05-12 20:05:43 -0300

    new class edge

--- edge.py
+++ edge.py
@@ -1,7 +1,15 @@
-class edges(set):
-    def get_edges(self):
-        return self
-    def set_inode(self, inode:int):
-        self.inode = inode
+class edges:
+    def __init__( self ):
+        self.inodes = set()
+        self.edges = set()
+    def add( self , edg: edge ):
+        self.inodes.add( edg.inode )
+        self.edges.add( edg )
+
     
-        
+class edge:
+    def __init__( self , args = None, inode = -1 ):
+        self.args = args
+        self.inode = inode
+    def set_inode( self , inode ):
+        self.inode = inode 
